35ce2b1e117b487967730f45cd17a35e10b2a5d6
movbou <movbo43@gmail.com>
2025-02-24 20:33:13 +0000

    Add nvim directory correctly

---
+++ .config/nvim/lua/configs/conform.lua
@@ -0,0 +1,34 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+	    c   = { "c_formatter_42" },
+    cpp = { "c_formatter_42" },
+  },
+
+
+  formatters = {
+    c_formatter_42 = {
+      inherit = false,  -- optional: disable merging with any defaults
+      command = "/home/gyro/.venv/bin/c_formatter_42",
+      args = { "$FILENAME" },  -- removed --stdin-filepath from here
+      stdin = false,  -- since the tool doesn’t support stdin input
+      cwd = function(ctx)
+        return vim.fn.getcwd()
+      end,
+      condition = function(ctx)
+        return vim.fn.executable("/home/gyro/.venv/bin/c_formatter_42") == 1
+      end,
+    },
+  },
+
+
+   format_on_save = {
+  --   -- These options will be passed to conform.format()
+  timeout_ms = 500,
+  lsp_fallback = true,
+  },
+}
+
+return options
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,13 @@
+require "nvchad.options"
+
+-- add yours here!
+
+vim.opt.number = true       -- Show absolute number for current line
+vim.opt.relativenumber = true -- Relative numbers for all other lines
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+-- 
+vim.opt.expandtab = false    -- Do not convert tabs to spaces
+vim.opt.tabstop = 4          -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 4       -- Number of spaces to use for each indentation step
+vim.opt.softtabstop = 4      -- Number of spaces a Tab counts for while editing
